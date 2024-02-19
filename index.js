4d3d4d31c20d76ba758cac21a6f6fbb5b869510b
Albin <riviere.albin@gmail.com>
2024-02-19 12:12:59 +0100

    First Commit

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ function ChangerTitre() {
     document.getElementById("titre").textContent = " Titre modifié !";
 }
 
+// Déclaration d'une variable
+//var, let, const
+
+//var : réaffecter, redéclarer, accéder de partout.
+var test = "test";
+
+//let : réaffecter, accéder de partout, pas redéclarer.
+let test2 = "test2";
+
+//const : pas réaffecter, pas redéclarer, accéder de partout.
+const test3 = "test3";
+
 ChangerTitre();
