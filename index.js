b4296c4bafb52460d0ad771b17e24af7eb4298a0
Albin <riviere.albin@gmail.com>
2024-02-19 12:40:05 +0100

    First Commit

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// Déclaration d'une variable
-//var, let, const
-
-//var : réaffecter, redéclarer, accéder de partout.
-var test = "test";
-
-//let : réaffecter, accéder de partout, pas redéclarer.
-let test2 = "test2";
-
-//const : pas réaffecter, pas redéclarer, accéder de partout.
-const test3 = "test3";
-
 // CamelCase
 const monTitre = document.getElementById("titre");
 
