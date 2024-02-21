f66ad252d374463c43165bab16bdf2ea59bec656
Albin <riviere.albin@gmail.com>
2024-02-21 18:20:06 +0100

    Projet liste_course

--- projet_marche/liste_cours.js
+++ projet_marche/liste_cours.js
@@ -1,23 +1,75 @@
 let basket = []
-let table = document.getElementById("tableau")
-let prix = {pomme : 0.5, banane : 1.3, orange : 0.75, papaye : 1.7, kiwi : 0.6, courgette : 1.2, potiron : 1.2, mangue : 1.5, kiwi : 0.9, pasteque : 2.0}
+prixTotal = 0
+let ElementRetire = document.getElementById("ElementRetire")
+
+function ajouterItem (basket, item, prixTotal, action) {
+
+    let price = {"Pomme" : 0.5, "Banane" : 1.5, "Orange" : 0.75, "Papaye" : 2, "Kiwi" : 0.5, "Courgette" : 1.5, "Potiron" : 1.5, "Salade" : 1.5, "Tomate" : 1, "PommeDeTerre" : 2.0, "Paprika" : 3.0, "Piment" : 1.5, "Gingembre" : 3.5, "Oignon" : 2.5, "Basilic" : 1.0 }
+    let table = document.getElementById("tableau")
+
+    if (action === "ajouter") {
+
+        // console.log(basket)
+        // console.log(item)
+        // console.log(table)
+        // console.log(price[item])
+
+        if (item in basket) {
+            basket[item] += 1
+            let row = table.rows.namedItem(item)
+
+            row.cells[1].innerText = basket[item]
+            row.cells[3].innerText = basket[item] * price[item] + " €"
 
 
-function ajouterItem (basket, item) {
+        }else{
+            // console.log(item)
+            basket.push(item)
+            basket[item] = 1
 
-    if (item in basket){
-        basket[item] += 1
+            let row = table.insertRow(1)
+            // console.log(row)
+            row.id = item
+            let nom = row.insertCell(0)
+            let quantite = row.insertCell(1)
+            row.insertCell(2)
+            row.insertCell(3)
 
-    }else{
-        basket.push(item)
-        basket[item] = 1
+            nom.innerText = item
+            quantite.innerText = basket[item]
 
+            // console.log(prix)
+            // console.log(basket[item])
+            // console.log(price[item])
+            // console.log(basket[item] * price[item])
+            row.cells[2].innerText = price[item] + " €"
+            row.cells[3].innerText = basket[item] * price[item] + " €"  
+        }
+
+        prixTotal += price[item]
+        // console.log(prixTotal)
+        document.getElementById("prixTotal").textContent = "Le prix total est de : " + prixTotal + " €";
+        // console.log(basket)
+
+    } else if (action === "retirer") {
+        while (table.rows.length > 1){
+            table.deleteRow(1);
+        }
+        basket.length = 0
+        prixTotal = 0
+        document.getElementById("prixTotal").textContent = "Le prix total est de : " + prixTotal + " €";
+
+        const props = Object.keys(basket)
+        for (const prop of props) {
+            delete basket[prop]
+        }
+        // for (const prop in basket) {
+        //     if (basket.hasOwnProperty(prop)) {
+        //         delete basket[prop];
+        //     }
+        // }
+    
+        // console.log(basket, prixTotal)
+        // console.log(basket.length)
     }
-    let row = table.insertRow(1)
-    let cell1 = row.insertCell(0)
-    let cell2 = row.insertCell(1)
-    cell1.innerText = item
-    cell2.innerText = basket[item]
-
-    console.log(basket)
 }
